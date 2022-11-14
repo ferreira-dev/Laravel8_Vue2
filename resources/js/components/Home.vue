f814a2c797113499379deaede2931fc211dcfc18
fabricio ferreira <fabricio.ramos@021dental.com.br>
2022-11-14 13:28:59 -0300

    [new] converte view Home em componente Vue

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Home - Dashboard</div>
+
+                <div class="card-body">
+                    <div class="alert alert-success" role="alert">
+                        Bem vindo!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component HOME.')
+        }
+    }
+</script>
